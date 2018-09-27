62852a2c7592ab263b1312b393ee280eeb778d51
Byteme <shvartsmand@gmail.com>
2018-09-27 00:37:54 -0700

    save

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,7 +7,7 @@ import React from "react";
 
 const About = () => (
   <div>
-    <body backgroundImage="https://i.imgur.com/qkdpN.jpg"/>
+
       <h1>Pupster</h1>
       <h2>They're the Good Boys and Girls</h2>
 
