6ca5de1f8ce705686a32adf79b8ae7ed814d3f00
Byteme <shvartsmand@gmail.com>
2018-10-05 16:00:34 -0700

    save

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,6 +3,19 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer">
+    <h5>Stay in touch!</h5>
+    <hr/>
+    <div className="contactIcons">
+      <i class="fab fa-linkedin"></i>
+      <i class="fab fa-github"></i>
+      <i class="fas fa-file"></i>
+      <i class="fab fa-google-plus"></i>
+      <i class="fab fa-codepen"></i>
+      <i class="fab fa-medium"></i>
+      <i class="fab fa-stack-overflow"></i>
+      <i class="fab fa-free-code-camp"></i>
+      <i class="fab fa-hackerrank"></i>
+    </div>
     <span>Made by Daniela Shvartsman &#169; </span>
   </footer>
 );
