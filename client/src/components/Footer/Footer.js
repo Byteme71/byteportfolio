11205e651eb598f5e794bafae378f92faaa114c8
Byteme <shvartsmand@gmail.com>
2018-10-11 12:11:52 -0700

    save

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Footer.css";
+// import { Link } from "react-router-dom";
 
 const Footer = () => (
   <footer className="footer">
     <h5>Stay in touch!</h5>
     <hr/>
     <div className="contactIcons">
-      <i class="fab fa-linkedin"></i>
-      <i class="fab fa-github"></i>
+      <a href="https://www.linkedin.com/in/shvartsmand/" target="_blank"><i class="fab fa-linkedin"></i></a>
+      <a href="https://github.com/Byteme71" target="_blank"><i class="fab fa-github"></i></a>
       <i class="fas fa-file"></i>
-      <i class="fab fa-google-plus"></i>
+      <a href="https://plus.google.com/u/0/+DanielaShvartsman" target="_blank"><i class="fab fa-google-plus"></i></a>
       <i class="fab fa-codepen"></i>
       <i class="fab fa-medium"></i>
       <i class="fab fa-stack-overflow"></i>
