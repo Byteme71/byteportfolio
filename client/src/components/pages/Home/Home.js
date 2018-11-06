90054cfecedd1b2920bf2d432127824c3d10de5f
Byteme <shvartsmand@gmail.com>
2018-11-05 17:39:03 -0800

    save

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -48,12 +48,12 @@ class Home extends React.Component {
             <LightSpeed right delay={1300}>
               <h1 className="initialText name">Shvartsman</h1>
               </LightSpeed>
-              </div>
+            </div>
        
           <Fade bottom cascade delay={2300}>
             <h4 className="thirdText">a Full Stack Web Developer</h4>
             <div className="centerBtn"> <Link className="noEffect" to="/about">
-              <button type="button" className="btn btn-outline-info">
+              <button type="button" className="btn btn-outline-info mediaResize">
                Click me to learn more!</button></Link>  
               </div>
           </Fade>
